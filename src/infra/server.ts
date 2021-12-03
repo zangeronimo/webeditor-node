ae82026d3f4162b4bb7f3863ecddfb476c4ed8f2
Luciano Zangeronimo <zangeronimo@gmail.com>
2021-12-03 18:04:58 -0300

    cors adjusts

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -16,9 +16,7 @@ import './container';
 
 const app = express();
 
-app.use(cors({
- origin: ['http://localhost:3000']
-}));
+app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use('/files', express.static(path.resolve(__dirname, '..', '..', 'upload')));
 app.use('/api-docs', swagger.serve, swagger.setup(swaggerJson));
